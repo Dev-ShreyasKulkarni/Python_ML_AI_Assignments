5833cffb8abe06f17ff9810ba3e0be09d72c73c9
Shreyas Kulkarni <shreyaskulkarni2212@gmail.com>
2026-07-03 22:12:07 +0530

    Assignment No 14

---
+++ Assignment_No14/Numbers.py
@@ -0,0 +1,19 @@
+Square = lambda x : x*x
+
+Cube = lambda x : x*x*x
+
+Max = lambda x,y : x if x>y else y
+
+Min = lambda x,y : x if x<y else y
+
+IsEven = lambda x : x%2==0
+
+IsOdd = lambda x : x%2!=0
+
+IsDivsibleBy5 = lambda x : x%5==0
+
+Addition = lambda x,y : x+y
+
+Multiplication = lambda x,y : x*y
+
+LargestOfThree = lambda x,y,z : x if x>y and x>z else (y if y>z else z)
